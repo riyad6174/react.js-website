c1bf60607627efe15080c97fa866e8565d778420
rahat <201853082050@nuist.edu.cn>
2021-09-25 23:15:19 +0600

    header and footer responsiveness added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './App.css'
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 function App() {
   return (
     <div >
       <Header />
+      <h1>break</h1>
+      <Footer />
     </div>
   );
 }
