eca32157f4012e778c981db0595643cd6798e12e
Riyad6174 <alimranhosen21@yahoo.com>
2021-09-26 01:28:34 +0600

    Route update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router,Switch, Route } from 'react-router-dom'
-import Home from './Components/Home/Home';
-import Docs from './Components/Docs/Docs';
-import Navbar from './Components/Navbar/Navbar';
+import RouterHandler from './Components/Routes/RouterHandler';
+
+
 
 function App() {
   return (
-    <Router>
-      <div >
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/docs" exact component={Docs} />
+    <>
 
+<RouterHandler/>
 
-
-        </Switch>
-
-        <p>De boldog a úgy gondom csereg le közt hoz nincsen, elszenderül nem jég ugassátok hű nem kábít. Szerelemben hegyét le hol gondom csizmák kerüljetek, hogy karja elevenek a nagyon ölel puszta menekülsz kapjatok le, ölel szeretni hoz lásd de ellökött kellene, kettős rázza ő kábít szenvedi jár ezért csörgő, kivül.</p>
-      </div>
-    </Router>
-  );
+    </>
+  )
 }
 
 export default App;
